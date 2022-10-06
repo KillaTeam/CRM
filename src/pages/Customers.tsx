fde234cf962d3aa934185618f92e12908ad1e433
Zmirro <dimonkings2018@gmail.com>
2022-10-07 00:13:49 +0200

    scssShadowFix

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import EarningList from '../components/EarningList';
-import dashLocals from "../data/Dashboard/Dashboard_item_money";
-import '../scss/Dashboard.scss'
 
 const Customers = () => {
   return (
