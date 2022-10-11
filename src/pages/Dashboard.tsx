77338312bc1cd2feed1ce1ab717f0a8b40f554c1
TOKYO <nezexmain@gmail.com>
2022-10-11 17:21:59 +0200

    Back up fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import EarningList from "../components/EarningList";
 import ShopList from "../components/ShopList";
 import dashLocals from "../data/Dashboard/Dashboard_item_money";
-import shopLocals from '../data/Dashboard/Shop_data'
+import shopLocals from "../data/Dashboard/Shop_data";
 import GraphBarQuart from "../components/GraphBarQuart";
 import { Dropdown } from "react-dropdown-now";
 import DonutChart from "react-donut-chart";
@@ -11,15 +11,9 @@ import "react-dropdown-now/style.css";
 import "../scss/Dashboard.scss";
 import "../scss/Graphs.scss";
 import "@progress/kendo-theme-default/dist/all.scss";
-<<<<<<< HEAD
 import "../scss/Shop.scss";
-import '../scss/Shop.scss'
+import "../scss/Shop.scss";
 import axios from "axios";
-
-=======
-import '../scss/Shop.scss'
->>>>>>> parent of 212d51f (123)
-
 
 
 // import arrow_overview_menu from "../assets/svg/vector.svg";
@@ -27,45 +21,39 @@ import '../scss/Shop.scss'
 // import GraphDon from "../components/GraphDon";
 // import NavGraphData from "../assets/NavGraphData";
 
-<<<<<<< HEAD
-=======
 const Main = () => {
+  const username = "Evano";
 
-  const [menuSearch, setMenuSeatch] = useState('Search');
->>>>>>> parent of 212d51f (123)
-
-
-const Main = () => {
-
-  const username = "Evano"
-
-  interface IDashData{
-    id: number,
-    name: string,
-    earning_money: number,
-    balance: number,
-    total_sales: number,
+  interface IDashData {
+    id: number;
+    name: string;
+    earning_money: number;
+    balance: number;
+    total_sales: number;
   }
 
-  const [DashData, setDashData] = useState<IDashData>()
+  const [DashData, setDashData] = useState<IDashData>();
 
-  const AxiosPostRequest = async() => {
+  const AxiosPostRequest = async () => {
     const options = {
-      method: 'POST',
-      url: 'http://localhost:5000/api/dashboard',
-      data: {name: username}
+      method: "POST",
+      url: "http://localhost:5000/api/dashboard",
+      data: { name: username },
     };
-    
-    axios.request(options).then(function (response: { data: IDashData; }) {
-      return setDashData(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
-  }
+
+    axios
+      .request(options)
+      .then(function (response: { data: IDashData }) {
+        return console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
   useEffect(() => {
-    AxiosPostRequest()
-  })
-  
+    AxiosPostRequest();
+  });
+
   const colors = [
     "#e91e63",
     "#9c27b0",
@@ -86,8 +74,6 @@ const Main = () => {
     "#607d8b",
   ];
 
-  
-
   return (
     <div style={{ flex: "2 0 50%" }} className="main_container">
       <div className="container_dashboard">
@@ -101,22 +87,6 @@ const Main = () => {
                   <h2>Overview</h2>
                   <p>Monthly Earning</p>
                 </div>
-<<<<<<< HEAD
-=======
-                
-                <Dropdown
-                  className="btn_dropdown"
-                  placeholder="Quartals"
-                  options={["Quartals", "3 Month", "Years"]}
-                  value="one"
-                  // onChange={(value) => console.log("change!", value)}
-                  // onSelect={(value) => console.log("selected!", value)}
-                  // onClose={(closedBySelection) =>
-                  //   console.log("closedBySelection?:", closedBySelection)
-                  // }
-                  // onOpen={() => console.log("open!")}
-                />
->>>>>>> parent of 212d51f (123)
               </div>
               <GraphBarQuart />
             </div>
@@ -126,58 +96,22 @@ const Main = () => {
                 <p>Customers that buy products</p>
               </div>
               <DonutChart
-                className="graph_don_item"
+                selectedOffset={0.04}
+                clickToggle={false}
+                strokeColor="#F8FAFE"
+                toggledOffset={0.05}
+                className="graph_don_item "
                 legend={false}
                 width={350}
                 height={350}
-                innerRadius={0.55}
+                innerRadius={0.45}
                 data={GraphDonData}
                 colors={colors}
                 // style={{border:}}
               />
-              
             </div>
           </div>
-<<<<<<< HEAD
           <ShopList shopLocals={shopLocals} />
-=======
-          <ShopList shopLocals={shopLocals}/>
-          {/* <div className="shop_container">
-
-            <div className="shop_menu">
-              <h2>Product Sell</h2>
-              <div className="shop_search">
-                <div className="nav_shop">
-                  <img src={svgShop} alt="svgShop" />
-                  <input type="text" value={menuSearch} onChange={e => setMenuSeatch('' + e.target.value)}/>
-                </div>
-                <select name="days_select" id="days_select">
-                  <option value="">Last 30 days</option>
-                </select>
-              </div>
-            </div>
-
-            <table className="shop_store_names">
-              <tr>
-                <th className="PName">Product Name</th>
-                <th className="SPT">Stock</th>
-                <th className="SPT">Price</th>
-                <th className="SPT">Total Sales</th>
-              </tr>
-              
-              <tr>
-                <td className="PName">
-                  <img src={img1} alt="img1" />
-                  <div className="title_desc"><h1>Absract 3D</h1> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> </div>
-                </td>
-                <td className="SPT SPT_item">32 in stock</td>
-                <td className="SPT SPT_item">$ 45.99</td>
-                <td className="SPT SPT_item">20</td>          
-              </tr>
-            </table>
-          </div> */}
->>>>>>> parent of 212d51f (123)
-
         </main>
       </div>
 
